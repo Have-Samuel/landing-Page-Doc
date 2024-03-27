6fc959860012c59112eb2fae745d498140074f55
Have Samuel <samiehave9@gmail.com>
2024-03-27 13:09:01 +0300

    Adding customized media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   darkMode: 'selector',
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'}, // 1.5
+        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'}, // 0.5
+      }
+    },
   },
   plugins: [],
 }
